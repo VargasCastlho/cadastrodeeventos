d1c3b76383131a4f668a3b7e969ba53fa09ad771
vargascastlho <fecastilhovargas@id.uff.br>
2023-09-20 18:35:09 -0300

    feat: feito exceptions

---
+++ src/main/java/org/vargascastlho/domain/exceptions/DataException.java
@@ -0,0 +1,8 @@
+package org.vargascastlho.domain.exceptions;
+
+public class DataException extends Exception {
+
+    public DataException(String messagem) {
+        super(messagem);
+    }
+}
---
+++ src/main/java/org/vargascastlho/domain/exceptions/ValidacaoException.java
@@ -0,0 +1,8 @@
+package org.vargascastlho.domain.exceptions;
+
+public class ValidacaoException extends Exception {
+
+    public ValidacaoException(String messagem) {
+        super(messagem);
+    }
+}
